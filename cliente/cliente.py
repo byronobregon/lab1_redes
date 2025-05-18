295dcdb6f335c225ea2ea69c5d795b2271916cc0
Enrique <85845008+erodriguezln@users.noreply.github.com>
2025-05-18 00:30:00 -0400

    Changes in the logic, still need refactoring and adjustments

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -1,9 +1,11 @@
 import socket
 import os
 import json
+import sys
 
 HOST = os.getenv("SERVER_HOST", "localhost")
 PORT = int(os.getenv("SERVER_PORT", "3000"))
+
 
 def menu():
     print("----- Bienvenido a Adivina el Número -----")
@@ -12,28 +14,54 @@ def menu():
     print("2 - Salir")
     return input()
 
+
 def start_game():
-    request_game_message = "1000" # TODO: 1000 indicates the request of a new game
-                                  # evaluate a better option (¿JSON?)
-    response = send_message(request_game_message)
-    # TODO: This message should be "OK" or "Error" #############################
-    print("Conectado al servidor: ", response["message"])
-    ############################################################################
-    play(response["attempts"])
+    request = {
+        "action": "start"
+    }
+
+    response = send_message(request)
+    if response.get("status") == "busy":
+        print("Server is busy right now.")
+    else:
+        print("Conectado al servidor: ", response["action"])
+        play(response["attempts"])
+
 
 def send_message(message):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall(message.encode())
+        s.sendall(json.dumps(message).encode())
         return json.loads(s.recv(1024).decode())
 
+
 def play(attempts):
-    # TODO: it's missing a message to stop recursion
-    # (servers should not end connection before client ask's for it)
     number = input(f"Quedan {attempts} intentos: Ingrese un número:\n")
-    response = send_message(number)
+
+    request = {
+        "action": "guess",
+        "number": str(number)
+    }
+    response = send_message(request)
     print("Respuesta: ", response["message"])
-    play(response["attempts"])
+
+    if response.get("status") in ["won", "no_attempts"]:
+        play_response = input(f"¿Desea jugar de nuevo? (s/n)\n").lower()
+        if play_response == "s":
+            request = {
+                "action": "start"
+            }
+            response = send_message(request)
+            play(response["attempts"])
+        elif play_response == "n":
+            request_stop = {"action": "stop"}
+            send_message(request_stop)
+    elif response.get("status") == "closing":
+        print("CLOSING")
+        sys.exit()
+    else:
+        play(response["attempts"])
+
 
 def main():
     selection = menu()
@@ -47,6 +75,7 @@ def main():
         else:
             print("Opción inválida, por favor seleccione 1 o 2.")
             selection = menu()
+
 
 if __name__ == "__main__":
     main()
--- intermedio/servidor_intermedio.py
+++ intermedio/servidor_intermedio.py
@@ -1,68 +1,80 @@
 import socket
 import os
 import json
+import sys
 
 TCP_HOST = '0.0.0.0'
 TCP_PORT = int(os.getenv("TCP_PORT", 3000))
-
 UDP_FINAL_HOST = os.getenv("FINAL_SERVER_HOST", "localhost")
+MAX_ATTEMPTS = 3
 
-
-# UDP_FINAL_PORT = int(os.getenv("FINAL_SERVER_PORT", 6000))
 
 def number_response(str):
-    if str == "bigger":
-        return "El número es mayor"
-    elif str == "smaller":
-        return "El número es menor"
-    else:
-        return "¡Has acertado!"
+    dict_responses = {
+        "bigger": "El número es mayor",
+        "smaller": "El número es menor",
+        "same": "¡Has acertado!"
+    }
+    return dict_responses.get(str)
+
+
+def send_udp_request(udp_sock, request_data, udp_port):
+    udp_sock.sendto(json.dumps(request_data).encode('utf-8'), (UDP_FINAL_HOST, udp_port))
+    response, _ = udp_sock.recvfrom(1024)
+    return json.loads(response.decode('utf-8'))
+
+
+def create_tcp_response(attempts, message, status):
+    return {
+        "action": "OK",
+        "attempts": attempts,
+        "message": message,
+        "status": status
+    }
 
 
 def main():
     # TODO Refactor this
     UDP_FINAL_PORT = 6000
-    attempts_count = 7
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_sock:
         tcp_sock.bind((TCP_HOST, TCP_PORT))
         tcp_sock.listen()
         print("Servidor intermedio escuchando TCP...")
+        attempts_count = MAX_ATTEMPTS
 
         while True:
             conn, addr = tcp_sock.accept()
             with conn:
-                number = conn.recv(1024).decode()
+                data = conn.recv(1024).decode()
+                request = json.loads(data)
+                action = request.get("action")
+                number = request.get("number")
                 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_sock:
-                    udp_sock.sendto(number.encode(), (UDP_FINAL_HOST, UDP_FINAL_PORT))
-                    response, _ = udp_sock.recvfrom(1024)
-                    json_response = json.loads(response.decode('utf-8'))
-                    # TODO refactor this
-                    UDP_FINAL_PORT = int(json_response.get("port"))
-                    print(json_response)
-                    if attempts_count <= 0:
-                        conn.sendall(b"No trys left")
-                        # TODO it should await from client message to close
-                        # after that it should tell the server to close the process
-                        # then it should receive an ok from the server, and
-                        # send a message to the client
-                        # after that it should close the process
-                        break
-                    elif number_response(json_response.get("message")) == "¡Has acertado!":
-                        conn.sendall(b"You won!")
-                        # TODO it should await from client message to close
-                        # after that it should tell the server to close the process
-                        # then it should receive an ok from the server, and
-                        # send a message to the client
-                        # after that it should close the process
-                        break
-                    client_message = {
-                        "message": number_response(json_response.get("message")),
-                        "attempts": attempts_count
-                    }
-                    print(number)
-                    conn.sendall(json.dumps(client_message).encode('utf-8'))
-                    if number != "1000": # TODO check with client request game condition
+                    if action == "start":
+                        udp_request = {"action": "start"}
+                        attempts_count = MAX_ATTEMPTS
+                    elif action == "guess":
+                        udp_request = {"action": "guess", "number": number}
                         attempts_count -= 1
+                    elif action == "stop":
+                        udp_request = {"action": "stop"}
+                    json_response = send_udp_request(udp_sock, udp_request, UDP_FINAL_PORT)
+                    json_status = json_response.get("status")
+                    print(json_response)
+
+                    if attempts_count <= 0:
+                        message = "No attempts left"
+                        response = create_tcp_response(attempts_count, message, "no_attempts")
+                        udp_request = {"action": "reset"}
+                        send_udp_request(udp_sock, udp_request, UDP_FINAL_PORT)
+                    elif json_status in ["playing", "won"]:
+                        message = number_response(json_response.get("message"))
+                        response = create_tcp_response(attempts_count, message, json_status)
+                    elif json_status == "closing":
+                        response = {"action": "OK", "status": "closing"}
+                        conn.sendall(json.dumps(response).encode('utf-8'))
+                        sys.exit()
+                    conn.sendall(json.dumps(response).encode('utf-8'))
 
 
 if __name__ == "__main__":
